09d91bc2311934cd22a26c1872fe0ed200394963
Emily Owaki <emily.owaki@gmail.com>
2014-11-28 13:28:52 -0600

    adding stubs so last 2 tests pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # helper ApplicationHelper
+
+  include ApplicationHelper
+
+  # def current_user
+  #   @user ||= User.find(session[:user_id])
+  # end
 end
--- spec/controllers/residence_controller_spec.rb
+++ spec/controllers/residence_controller_spec.rb
@@ -22,7 +22,10 @@ describe ResidencesController do
     context "when valid params are passed" do
       it 'assigns a new residence as @residence' do
         residence_params = {residence: {city: "Chicago", state: "IL", zip_code: "12345", description: "It's hella decent"} }
-        # current_user.id = 4
+
+        user = User.create(name: "Nate Delage", email: "natelovescats@gmail.com", password: "catscats")
+        allow(controller).to receive(:current_user) {user}
+
         post :create, residence_params
         expect(assigns(:residence)).to eq(Residence.last)
       end
@@ -30,12 +33,16 @@ describe ResidencesController do
     context "when invalid params are passed" do
       it "assigns a newly created but unsaved residence as @residence" do
         residence_params = {residence: {state: "IL", zip_code: "12345", description: "It's hella decent"} }
+        user = User.create(name: "Nate Delage", email: "natelovescats@gmail.com", password: "catscats")
+        allow(controller).to receive(:current_user) {user}
         post :create, residence_params
         expect(assigns(:residence)).to be_a_new(Residence)
       end
 
       it 'renders the new template after attempt' do
         residence_params = {residence: {state: "IL", zip_code: "12345", description: "It's hella decent"} }
+        user = User.create(name: "Nate Delage", email: "natelovescats@gmail.com", password: "catscats")
+        allow(controller).to receive(:current_user) {user}
         post :create, residence_params
         expect(response).to render_template("new")
       end
