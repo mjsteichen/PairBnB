5e0ccbd47b34f6778a46831af6e39bb9824f8f42
Emily Owaki <emily.owaki@gmail.com>
2014-11-29 14:41:59 -0600

    adds flash.now to sorrty no pairs hick search

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -6,7 +6,7 @@ class ResidencesController < ApplicationController
   def search
     @residences = Residence.where(city: params[:search])
     if @residences.length == 0
-      flash[:notice] = "Sorry! No pairs in that neck of the woods."
+      flash.now[:notice] = "Sorry! No pairs in that neck of the woods."
     end
   end
 
