f8682a2211e5547e983c91d7c8a3d2f5f1466d95
Emily Owaki & Nick Brown <pair+jabbowaki.nick-brown@devbootcamp.com>
2014-11-29 11:42:01 -0600

    got calendar on page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,4 +38,38 @@ $(document).ready(function() {
     $(".displays").hide();
     $("#user_information").show();
   });
+
+  YUI().use('calendar', 'datatype-date', 'cssbutton',  function(Y) {
+
+    // Create a new instance of calendar, placing it in
+    // #mycalendar container, setting its width to 340px,
+    // the flags for showing previous and next month's
+    // dates in available empty cells to true, and setting
+    // the date to today's date.
+    var calendar = new Y.Calendar({
+      contentBox: "#mycalendar",
+      width:'340px',
+      showPrevMonth: true,
+      showNextMonth: true,
+      date: new Date()}).render();
+
+    // Get a reference to Y.DataType.Date
+    var dtdate = Y.DataType.Date;
+
+    // Listen to calendar's selectionChange event.
+    calendar.on("selectionChange", function (ev) {
+
+      // Get the date from the list of selected
+      // dates returned with the event (since only
+      // single selection is enabled by default,
+      // we expect there to be only one date)
+      var newDate = ev.newSelection[0];
+
+      // Format the date and output it to a DOM
+      // element.
+      Y.one("#selecteddate").setHTML(dtdate.format(newDate));
+    });
+  });
 });
+
+
