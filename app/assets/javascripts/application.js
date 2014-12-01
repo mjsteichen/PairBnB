97e8e210a567bf1ed0b0126f3faa22fc2be8c3a5
Nick Brown <nick.brown2014@gmail.com>
2014-12-01 15:51:21 -0600

    adds toggle to login on landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+  $('#loginbar').on('click',function(){
+    $('#login-landing').toggle();
+  });
+});
