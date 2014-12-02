2f125afa523994204c742c2e63a187d196adc7bb
Emily Owaki <emily.owaki@gmail.com>
2014-12-02 09:26:27 -0600

    adds model tests for images and requests, reorganizes all tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 describe Message do
-  it { should validate_presence_of(:sender_id) }
-  it { should validate_presence_of(:recipient_id) }
-  it { should validate_presence_of(:text) }
-  it { should belong_to(:sender) }
-  it { should belong_to(:recipient) }
+  describe "#validations" do
+    it { should validate_presence_of(:sender_id) }
+    it { should validate_presence_of(:recipient_id) }
+    it { should validate_presence_of(:text) }
+  end
+  describe "#associations" do
+    it { should belong_to(:sender) }
+    it { should belong_to(:recipient) }
+  end
 end
--- spec/models/pairing_spec.rb
+++ spec/models/pairing_spec.rb
@@ -5,6 +5,11 @@ describe Pairing do
   let(:michael) { User.create!(name: "michael", email: "michael@michael.com", password: "password")}
   let(:test_pairing) { Pairing.new(host_id: emily.id, visitor_id: michael.id) }
 
+  describe "#associations" do
+    it { should belong_to(:host) }
+    it { should belong_to(:visitor) }
+  end
+
   describe "#validations" do
     it 'should save when params are valid' do
       expect(test_pairing.save).to be_truthy
@@ -20,6 +25,8 @@ describe Pairing do
       expect(@bad_pairing.save).to be_falsey
     end
 
+    it { should validate_presence_of(:host_id) }
+    it { should validate_presence_of(:visitor_id) }
   end
 
 end
