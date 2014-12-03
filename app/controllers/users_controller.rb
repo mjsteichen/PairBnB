32bbb92e88eea61486f4f235e4c4bf5b373e240d
Emily Owaki <emily.owaki@gmail.com>
2014-12-03 11:12:46 -0600

    changing user update to not require password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,8 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password], picture_url: params[:user][:picture_url], github_url: params[:user][:github_url], twitter_url: params[:user][:twitter_url], bio: params[:user][:bio])
+    @user.attributes = {name: params[:user][:name], email: params[:user][:email], picture_url: params[:user][:picture_url], github_url: params[:user][:github_url], twitter_url: params[:user][:twitter_url], bio: params[:user][:bio]}
+    if @user.save(:validate => false)
       redirect_to user_path(@user)
     else
       render 'edit'
